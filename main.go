57b6e3e5b3cc5142c7f07ae30a48190944de6b60
Asbjørn Apeland <aude@users.noreply.github.com>
2019-05-06 17:34:06 +0200

    initial code

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-    "fmt"
+    "log"
+    "net/http"
+
+    "golang.org/x/net/webdav"
 )
 
 func main() {
-    fmt.Println("hello world")
+    dir := "."
+    port := "8080"
+
+    handler := &webdav.Handler{
+        FileSystem: webdav.Dir(dir),
+        LockSystem: webdav.NewMemLS(),
+        Logger: func(request *http.Request, err error) {
+            if err != nil {
+                log.Printf("[%s]: %s, ERROR: %s\n",
+                    request.Method,
+                    request.URL,
+                    err)
+            } else {
+                log.Printf("[%s]: %s\n",
+                    request.Method,
+                    request.URL)
+            }
+        },
+    }
+    http.Handle("/", handler)
+
+    log.Fatal(http.ListenAndServe(":" + port, nil))
 }
