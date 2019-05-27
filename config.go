421f2344f4b643430b57bc8046b7d40bba2b6c82
Asbjørn Apeland <aude@users.noreply.github.com>
2019-05-27 22:44:06 +0200

    add basic config support

---
+++ config.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "os"
+    "fmt"
+)
+
+type Config struct {
+    Port string
+    Dir  string
+}
+
+func NewConfig() (*Config, error) {
+    config := new(Config)
+
+    envConfig, err := parseEnv()
+    if err != nil {
+        return envConfig, fmt.Errorf("could not parse env variables: %s", err)
+    }
+
+    config = envConfig
+
+    return config, nil
+}
+
+func parseEnv() (*Config, error) {
+    config := new(Config)
+    var value   string
+    var success bool
+
+    if value, success = os.LookupEnv("PORT"); !success {
+        return config, fmt.Errorf("%s is not set", "PORT")
+    }
+    config.Port = value
+    if value, success = os.LookupEnv("DIR"); !success {
+        return config, fmt.Errorf("%s is not set", "DIR")
+    }
+    config.Dir = value
+
+    return config, nil
+}
